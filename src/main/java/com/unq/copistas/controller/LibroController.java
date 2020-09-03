2be5d64da56e8577534b33cbb0f256c6e57bc6e1
Miloro <miloromiguel@gmail.com>
2020-09-03 15:59:43 -0300

    build API basica (update y buscar por id)

--- src/main/java/com/unq/copistas/controller/LibroController.java
+++ src/main/java/com/unq/copistas/controller/LibroController.java
@@ -1,8 +1,10 @@
 package com.unq.copistas.controller;
 
+import com.unq.copistas.exception.ResourceNotFoundException;
 import com.unq.copistas.model.Libro;
 import com.unq.copistas.service.LibroService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -23,5 +25,21 @@ public class LibroController {
     @GetMapping("/libros")
     public List<Libro> getAllLibros(){
         return libroService.getAllLibros();
+    }
+
+    @GetMapping("libro/{id}")
+    public ResponseEntity<Libro> getLibroPorId(@PathVariable(value = "id") Long libroId) throws ResourceNotFoundException{
+        Libro libro = libroService.getUserById(libroId);
+        return ResponseEntity.ok().body(libro);
+    }
+
+    @PutMapping("/libro/{id}")
+    public ResponseEntity<Libro> updateLibro(
+            @PathVariable(value = "id") Long libroId,
+            @Valid @RequestBody Libro libroDetails)
+            throws ResourceNotFoundException {
+        System.out.print(libroId);
+        final Libro updatedLibro = libroService.updatelibro(libroId, libroDetails);
+        return ResponseEntity.ok(updatedLibro);
     }
 }
--- src/main/java/com/unq/copistas/service/LibroService.java
+++ src/main/java/com/unq/copistas/service/LibroService.java
@@ -1,11 +1,15 @@
 package com.unq.copistas.service;
 
 
+import com.unq.copistas.exception.ResourceNotFoundException;
 import com.unq.copistas.model.Libro;
 import com.unq.copistas.repository.LibroRepository;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.lang.module.ResolutionException;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -22,6 +26,27 @@ public class LibroService {
         return libroRepository.findAll();
     }
 
+    public Libro getUserById(Long libroId) throws ResourceNotFoundException {
+        Libro libro = libroRepository.findById(libroId).orElseThrow(()-> new ResourceNotFoundException("libro no encontrado con la id" + libroId));
+        return libro;
+
+    }
 
 
+    public Libro updatelibro(Long libroId, Libro libroDetails) throws ResourceNotFoundException {
+        Libro libro =
+                libroRepository
+                        .findById(libroId)
+                        .orElseThrow(() -> new ResourceNotFoundException("User not found on :: " + libroId));
+
+        libro.setTitulo(libroDetails.getTitulo());
+        libro.setNombreAutor(libroDetails.getNombreAutor());
+        libro.setApellidoAutor(libroDetails.getApellidoAutor());
+        libro.setEditorial(libroDetails.getEditorial());
+        libro.setEdicion(libroDetails.getEdicion());
+        libro.setIdioma(libroDetails.getIdioma());
+        libro.setCategoria(libroDetails.getCategoria());
+        final Libro updatedLibro = libroRepository.save(libro);
+        return updatedLibro;
+    }
 }
