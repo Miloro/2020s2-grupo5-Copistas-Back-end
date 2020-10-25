dd6b08f2a89fd3664753a7a42cf761b9c78d8261
ferblanco75 <ferblanco@gmail.com>
2020-10-25 11:02:02 -0300

    subo implementacion del modulo de jwt

--- src/main/java/com/unq/copistas/security/jwt/JwtEntryPoint.java
+++ src/main/java/com/unq/copistas/security/jwt/JwtEntryPoint.java
@@ -1,4 +1,29 @@
 package com.unq.copistas.security.jwt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
-public class JwtEntryPoint {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//esta clase se fija si hay un token valido
+//si no , devuelve un 401 unauthorized
+@Component
+public class JwtEntryPoint implements AuthenticationEntryPoint {
+
+
+    private final static Logger logger = LoggerFactory.getLogger(JwtEntryPoint.class);
+    @Override
+    public void commence(HttpServletRequest req, HttpServletResponse res, AuthenticationException e) throws IOException, ServletException {
+        //esto se utiliza solo en desarrollo
+        logger.error("failed en el metodo commence");
+        res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "no autorizado");
+    }
+
+
 }
--- src/main/java/com/unq/copistas/security/jwt/JwtTokenFilter.java
+++ src/main/java/com/unq/copistas/security/jwt/JwtTokenFilter.java
@@ -1,4 +1,48 @@
 package com.unq.copistas.security.jwt;
 
-public class JwtTokenFilter {
+import com.unq.copistas.security.service.UserDetailsServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//esta clase se ejecuta por cada peticion
+//si está ok da acceso al recurso
+public class JwtTokenFilter extends OncePerRequestFilter{
+    private final static Logger logger = LoggerFactory.getLogger(JwtTokenFilter.class);
+
+    JwtProvider jwtProvider;
+    UserDetailsServiceImpl userDetailsService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
+        //si el token es valido, provee el acceso al recurso
+        try{
+            String token = getToken(req);
+            if (token != null && jwtProvider.validateToken(token) ){
+                String nombreUsuario = jwtProvider.getNombreUsuarioFromToken(token);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(nombreUsuario);
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+        catch(Exception e){
+            logger.error("fail en el metodo doFIlter");
+
+        }
+        filterChain.doFilter(req,res);
+    }
+    private String getToken(HttpServletRequest request){
+        String header = request.getHeader("Authorization");
+        if (header != null && header.startsWith("Bearer"))
+                return header.replace("Bearer","");
+        return  null;
+    }
 }
